0639694aa6281c540ba2d8126f028f0f177e7c81
Michał Mleczko <michal@mleczko.dev>
2025-05-09 16:04:00 +0200

    fix: Rector config

--- rector.php
+++ rector.php
@@ -6,7 +6,6 @@ use Rector\Config\RectorConfig;
 
 return RectorConfig::configure()
     ->withPaths([
-        __DIR__.'/assets',
         __DIR__.'/config',
         __DIR__.'/public',
         __DIR__.'/src',
