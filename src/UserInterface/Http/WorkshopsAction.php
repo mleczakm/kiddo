3545a4c7715f56885a693dd40f579e952a268966
Michał Mleczko <michal@mleczko.dev>
2025-05-13 19:20:12 +0200

    feat: Add twig linter

--- src/UserInterface/Http/WorkshopsAction.php
+++ src/UserInterface/Http/WorkshopsAction.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\UserInterface\Http;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use function Symfony\Component\Translation\t;
 
@@ -14,7 +15,7 @@ class WorkshopsAction extends AbstractController
         'pl' => '/warsztaty',
         'en' => 'workshops',
     ], name: 'workshops')]
-    public function __invoke()
+    public function __invoke(): Response
     {
         return $this->render(
             'workshops.html.twig',
