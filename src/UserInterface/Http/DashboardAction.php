9c3788fbb70dabafc846496b14dac003298ad12c
Michał Mleczko <michal@mleczko.dev>
2025-05-19 15:37:30 +0200

    feat: Dashboard POC

--- src/UserInterface/Http/DashboardAction.php
+++ src/UserInterface/Http/DashboardAction.php
@@ -7,10 +7,16 @@ namespace App\UserInterface\Http;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 class DashboardAction extends AbstractController
 {
-    #[Route(path: '/panel', name: 'dashboard')]
+    #[Route(path: [
+        'en' =>  '/dashboard',
+        'pl' => '/panel',
+    ]
+        , name: 'dashboard')]
+    #[IsGranted('ROLE_USER')]
     public function __invoke(): Response
     {
         return $this->render(
