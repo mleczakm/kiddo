0fc7bfd7a230df401ec7d8d538314454a882a90d
Michał Mleczko <michal@mleczko.waw.pl>
2025-07-20 01:06:06 +0200

    feat: Add custom access denied handler and enhance security configuration
    - Introduce `AccessDeniedHandler` to redirect unauthorized users to the login page.
    - Enable `remember_me` functionality with updated configurations in `security.yaml`.
    - Add access control for `^/panel` paths requiring full authentication.

---
+++ src/Security/AccessDeniedHandler.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Security;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+
+readonly class AccessDeniedHandler implements EventSubscriberInterface
+{
+    public function __construct(
+        private UrlGeneratorInterface $router
+    ) {}
+
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            // the priority must be greater than the Security HTTP
+            // ExceptionListener, to make sure it's called before
+            // the default exception listener
+            KernelEvents::EXCEPTION => ['onKernelException', 2],
+        ];
+    }
+
+    public function onKernelException(ExceptionEvent $event): void
+    {
+        $exception = $event->getThrowable();
+
+        if (! $exception instanceof AccessDeniedException) {
+            return;
+        }
+
+        $path = $event->getRequest()
+            ->getPathInfo();
+
+        if (! str_starts_with($path, $this->router->generate('dashboard')) && ! str_starts_with(
+            $path,
+            $this->router->generate('admin')
+        )) {
+            return;
+        }
+
+        $event->setResponse(new RedirectResponse($this->router->generate('app_login')));
+    }
+}
