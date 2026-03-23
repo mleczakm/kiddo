5044fcaba6d0d64c08bb98b5ee0fecf9ab006ede
Michał Mleczko <michal@mleczko.waw.pl>
2026-03-23 01:29:05 +0100

    chore: update `.idea` config, and comment out unused Swoole kernel

--- src/Kernel.php
+++ src/Kernel.php
@@ -4,10 +4,12 @@ declare(strict_types=1);
 
 namespace App;
 
+// use SwooleBundle\SwooleBundle\Bridge\Symfony\Kernel\CoroutinesSupportingKernel;
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 
 class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
+    // use CoroutinesSupportingKernel;
 }
